90968a6387a98d1c8945c570dcefd896c07cb015
Hamza <hamza.ahmad2612@gmail.com>
2023-05-12 16:32:04 +0200

    made frontend more readable

--- client/src/App.js
+++ client/src/App.js
@@ -18,31 +18,23 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const database = getDatabase(
-  app,
-  process.env.REACT_APP_DATABASE
-);
+const database = getDatabase(app, process.env.REACT_APP_DATABASE);
 const storage = getStorage();
 
+const downloadAudio = async (path) => {
+  const uri = await getDownloadURL(sref(storage, path));
+
+  const sound = new Howl({
+    src: [uri],
+    html5: true,
+  });
+  sound.load();
+  sound.play();
+  await timeout(5000);
+  sound.stop();
+};
+
 function App() {
-  const downloadAudio = async (path) => {
-    const uri = await getDownloadURL(sref(storage, path));
-
-    const sound = new Howl({
-      src: [uri],
-      html5: true,
-    });
-    sound.load();
-    sound.play();
-    await timeout(5000);
-    sound.stop();
-  };
-  //let date = "May 10, 2023";
-  //let id = 0;
-
-  //writeSong("Homesick by Dayseeker", id, date, "songs/file_example_MP3_700KB.mp3");
-  //console.log('wrote song')
-
   return (
     <div>
       <button onClick={() => downloadAudio("songs/file_example_MP3_700KB.mp3")}>
@@ -55,13 +47,5 @@ function App() {
 function timeout(delay) {
   return new Promise((res) => setTimeout(res, delay));
 }
-
-//function writeSong(name, id, date, path) {
-//  set(ref(database, "songs/" + id), {
-//    name: name,
-//    date: date,
-//    path: path
-//  });
-//}
 
 export default App;
